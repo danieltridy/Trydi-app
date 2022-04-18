77f0c7e06019fe0cfc95a0641363197423303278
Jonathan Rincon <jonathan.rincon@red6ar.com>
2022-04-18 17:10:10 -0500

    Cosas que quedaron volando

---
+++ Assets/Scripts/Interface/CreateARObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateARObject : MonoBehaviour
+{
+    public static CreateARObject Instance;
+  
+    [SerializeField]
+    private MeshEditor meshEditor;
+
+    private string estru;
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void AddInformation(string estru) {
+        this.estru = estru;
+    }
+    public void CreateTridyEstru() {
+
+        meshEditor.JsonString = estru;
+        meshEditor.CreateMeshFromJson();
+    }
+    
+}
